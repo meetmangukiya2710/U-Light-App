0a21bcad73a89e89bcd5874e512b5010b2274141
Meet Mangukiya <meetmangukiya2710@gmail.com>
2024-03-22 10:53:45 +0530

    CollectionView Commit

---
+++ U-Light App/PropularProductCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PropularProductCollectionViewCell.swift
+//  U-Light App
+//
+//  Created by R95 on 22/03/24.
+//
+
+import UIKit
+
+class PropularProductCollectionViewCell: UICollectionViewCell {
+    
+}
